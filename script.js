36e934d4f594db9b4c4c54d684d7de9801ac2d61
Kuber <kishore.kuber23@vit.edu>
2024-09-01 12:23:31 +0530

    Add complete logic, including new sketch
    The project has been completed, with three buttons for random colours,
    progressive darkening and an eraser. In addition, there is a button for
    creating a new sketch

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const container = document.querySelector("#container");
-container.style.cssText = "display: flex; flex-direction: column;"
 
 function randomColor()
 {
@@ -11,6 +10,8 @@ function randomColor()
     return "rgb(" + r() + ", " + r() + ", " + r() + ")";
 }
 
+// Create a 16x16 grid for painting
+// box-sizing: border-box is used so that the grid does not overflow the container
 for (let i = 0; i < 16; i++)
 {
     let innerContainer = document.createElement("div");
@@ -19,8 +20,8 @@ for (let i = 0; i < 16; i++)
     for (let j = 0; j < 16; j++)
     {
         let squareDiv = document.createElement("div");
-        squareDiv.setAttribute("class", "square-div")
-        squareDiv.style.cssText = "width: 30px; height: 30px; border: 1px solid black; background-color: white;"
+        squareDiv.setAttribute("class", "square-div");
+        squareDiv.style.cssText = "width: 30px; height: 30px; border: 1px solid black; background-color: white; box-sizing: border-box";
         innerContainer.appendChild(squareDiv);
     }
     container.appendChild(innerContainer); 
@@ -56,28 +57,70 @@ eraserButton.addEventListener("click", () => {
     buttonChoice = 2;
 });
 
-document.querySelectorAll(".square-div").forEach((div) => {
-    div.addEventListener("mouseover", () => {
+// Event delegation is utilized here, as the event is attached to the container, which means that the new grids will also be able to be coloured
+// If we use the event listener on the square divs themselves, the event listener won't work for the new square divs in the new grids
+container.addEventListener("mouseover", (event) => {
+    if (event.target.classList.contains("square-div"))
+    {
+        let div = event.target;
         if (buttonChoice == 0) 
         {
             div.style.backgroundColor = randomColor();
-        }
-        else if (buttonChoice == 1)
+        } 
+        else if (buttonChoice == 1) 
         {
-            if (div.style.backgroundColor == "white")
+            if (div.style.backgroundColor == "white") 
             {
                 div.style.backgroundColor = randomColor();
                 div.style.opacity = 0.3;
             }
-            else if (div.style.opacity < 1)
+            else if (div.style.opacity < 1) 
             {
                 div.style.opacity = (parseFloat(div.style.opacity) + 0.1).toString();
             }
-        }
-        else if (buttonChoice == 2 && div.style.backgroundColor != "white")
+        } 
+        else if (buttonChoice == 2 && div.style.backgroundColor != "white") 
         {
             div.style.backgroundColor = "white";
+            // opacity is reassigned, else the opacity will remain at 0.3 and the grid won't look "erased"
             div.style.opacity = "1";
         }
-    });
-});
+    }
+})
+
+const newSketch = document.querySelector("#new-sketch");
+newSketch.addEventListener("click", createNewSketch);
+
+function createNewSketch()
+{
+    let size = prompt("Enter size of new grid (maximum 100): ", "16");
+    while (!(size !== null && +size >= 1 && +size <= 100))
+    {
+        size = prompt("Enter valid size: ", "16");
+    }
+
+    while (container.hasChildNodes())
+    {
+        container.removeChild(container.firstElementChild);
+    }
+
+    // A new grid is created similar to above, except here the length of the boxes depends on the user input
+    size = +size; // Convert size from string to number
+    let side = (480 / size) + "px"; // side is a string becuase of concatenation
+    for (let i = 0; i < size; i++)
+    {
+        innerContainer = document.createElement("div");
+        innerContainer.setAttribute("class", "inner-container");
+        innerContainer.style.cssText = "display: flex";
+
+        for (let j = 0; j < size; j++)
+        {
+            let squareDiv = document.createElement("div");
+            squareDiv.setAttribute("class", "square-div");
+            squareDiv.style.cssText = `width: ${side}; height: ${side};border: 1px solid black; background-color: white; box-sizing: border-box;`;
+            innerContainer.appendChild(squareDiv);
+        }
+
+        container.appendChild(innerContainer); 
+    }
+}
